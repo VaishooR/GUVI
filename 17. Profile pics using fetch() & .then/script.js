245c75020220cfe46c77047004752fbcff378045
VaishooR <96234429+VaishooR@users.noreply.github.com>
2022-12-16 17:11:41 +0530

    Update script.js

--- 17. Profile pics using fetch() & .then/script.js
+++ 17. Profile pics using fetch() & .then/script.js
@@ -22,3 +22,32 @@ fetch('https://62ec04c755d2bd170e7b4d17.mockapi.io/users')
   })
   
 })
+
+
+
+
+
+-------------------------------------------------------
+  
+  
+  
+  
+function createUser(person){
+  const card=document.createElement('div');
+  card.setAttribute('class','card');
+  card.innerHTML=`<img src='${person.avatar}'/><h1>${person.name}</h1>`;
+  document.body.append(card);
+}
+
+function getUser(){
+  fetch('https://62ec04c755d2bd170e7b4d17.mockapi.io/users')
+  .then((data)=>data.json())
+  .then((result)=>{
+  result.forEach((person)=>createUser(person))
+})
+  
+}
+getUser()
+
+
+
