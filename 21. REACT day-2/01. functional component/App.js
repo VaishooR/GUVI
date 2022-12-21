b9ee9d7c7c8821e28bef35ca12099271eb8ca073
VaishooR <96234429+VaishooR@users.noreply.github.com>
2022-12-21 21:08:51 +0530

    Create App.js

---
+++ 21. REACT day-2/01. functional component/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      <h1>First App</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
+export default App;
