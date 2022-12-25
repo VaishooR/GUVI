0076d88796672de40eb5c903f005f375a9d12c4c
VaishooR <96234429+VaishooR@users.noreply.github.com>
2022-12-25 12:42:42 +0530

    Update App.js

--- 25. REACT day-6/03. Higher Order Component/App.js
+++ 25. REACT day-6/03. Higher Order Component/App.js
@@ -8,3 +8,21 @@ function App() {
   );
 }
 export default HOC(App)                   <-----  App component is being sent as an input to the Higher Order component.
+
+
+
+
+
+
+
+
+import HOC from './HigherOrderComp';
+
+function App(props) {
+  return (
+    <div>
+      <h3>App comp content with props from index.js- {props.office} from {props.city}</h3>
+    </div>
+  );
+}
+export default HOC(App)
